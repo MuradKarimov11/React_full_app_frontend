418616feae580f847e80897048536e2bb8b4cf0c
Murad Karimov <muradkarimov07@gmail.com>
2024-10-08 23:49:33 +0300

    modified postsApi.ts file

--- src/app/services/postsApi.ts
+++ src/app/services/postsApi.ts
@@ -1,45 +1,45 @@
-import type { Post } from "../types";
-import { api } from "./api";
+import type { Post } from "../types"
+import { api } from "./api"
 
 export const postApi = api.injectEndpoints({
-    endpoints: (builder) => ({
-        createPost: builder.mutation<Post, {content: string}>({
-            query: (postData) => ({
-                url: '/posts',
-                method: 'POST',
-                body: postData
-            })
-        }),
-        getAllPosts: builder.query<Post[], void>({
-            query: () => ({
-                url: '/posts',
-                method: 'POST'
-            })
-        }),
-        getPostById: builder.query<Post, string>({
-            query: (id) => ({
-                url: `/posts/${id}`,
-                method: 'GET'
-            })
-        }),
-        deletePost: builder.mutation<void, string>({
-            query: (id) => ({
-                url: `/posts/${id}`,
-                method: 'DELETE'
-            })
-        })
-    })
+  endpoints: builder => ({
+    createPost: builder.mutation<Post, { content: string }>({
+      query: postData => ({
+        url: "/posts",
+        method: "POST",
+        body: postData,
+      }),
+    }),
+    getAllPosts: builder.query<Post[], void>({
+      query: () => ({
+        url: "/posts",
+        method: "GET",
+      }),
+    }),
+    getPostById: builder.query<Post, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "GET",
+      }),
+    }),
+    deletePost: builder.mutation<void, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
 })
 
 export const {
-    useCreatePostMutation,
-    useGetAllPostsQuery,
-    useGetPostByIdQuery,
-    useDeletePostMutation,
-    useLazyGetAllPostsQuery,
-    useLazyGetPostByIdQuery
-} = postApi;
+  useCreatePostMutation,
+  useGetAllPostsQuery,
+  useGetPostByIdQuery,
+  useDeletePostMutation,
+  useLazyGetAllPostsQuery,
+  useLazyGetPostByIdQuery,
+} = postApi
 
 export const {
-    endpoints: {createPost, getAllPosts, getPostById, deletePost}
-} = postApi;
+  endpoints: { createPost, getAllPosts, getPostById, deletePost },
+} = postApi
